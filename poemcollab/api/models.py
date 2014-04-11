08f5dfc3c111b56e8e3374e80436cdef9001308a
Geoffrey Sechter <geoffrey.sechter@gmail.com>
2014-04-10 20:01:07 -0400

    initial models draft

--- poemcollab/api/models.py
+++ poemcollab/api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from model_utils.models import TimeStampedModel
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+
+class User(TimeStampedModel, AbstractUser):
+    pass
+
+
+class Poem(TimeStampedModel):
+    owner = models.ForeignKey('User', null=True, related_name="poems")
+
+    def collaborators(self):
+        return User.objects.filter(poem_lines__in=self.lines.all())
+
+
+class PoemLine(TimeStampedModel):
+    poem = models.ForeignKey('Poem', null=True, related_name="lines")
+    author = models.ForeignKey('User', null=True, related_name="poem_lines")
+    text = models.CharField(max_length=512)
